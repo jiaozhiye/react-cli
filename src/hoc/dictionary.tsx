97585930ea6a65ef60d4a92f8f403a5c6371f2a2
焦质晔 <jiaozhiye@163.com>
2021-07-18 22:19:28 +0800

    1234

--- src/hoc/dictionary.tsx
+++ src/hoc/dictionary.tsx
@@ -2,14 +2,13 @@
  * @Author: 焦质晔
  * @Date: 2021-07-18 19:57:39
  * @Last Modified by: 焦质晔
- * @Last Modified time: 2021-07-18 21:27:32
+ * @Last Modified time: 2021-07-18 22:17:55
  */
 import React, { Component } from 'react';
 import hoistStatics from 'hoist-non-react-statics';
 import { connect } from 'react-redux';
 
 export default (WrappedComponent): any => {
-  @connect((state: any) => ({ dict: state.app.dict }), {})
   class C extends Component<any> {
     static displayName = `Dict(${WrappedComponent.displayName || WrappedComponent.name})`;
 
@@ -28,5 +27,5 @@ export default (WrappedComponent): any => {
     }
   }
 
-  return hoistStatics(C, WrappedComponent);
+  return connect((state: any) => ({ dict: state.app.dict }), {})(hoistStatics(C, WrappedComponent));
 };
