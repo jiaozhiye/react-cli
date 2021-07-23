5ab7f8e8cf50079d9ae95957f73d419fa7d568ec
焦质晔 <jiaozhiye@163.com>
2021-07-23 16:33:26 +0800

    1234

--- src/bootstrap.tsx
+++ src/bootstrap.tsx
@@ -2,10 +2,10 @@
  * @Author: 焦质晔
  * @Date: 2021-02-05 09:13:33
  * @Last Modified by: 焦质晔
- * @Last Modified time: 2021-07-05 15:56:15
+ * @Last Modified time: 2021-07-23 13:41:51
  */
-import * as React from 'react';
-import * as ReactDOM from 'react-dom';
+import React from 'react';
+import ReactDOM from 'react-dom';
 import App from './App';
 
 ReactDOM.render(<App />, document.getElementById('app'));
