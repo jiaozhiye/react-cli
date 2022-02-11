0d8d851abd58c75e3f1612d166abe56521c6a759
jzy <jiaozhiye@163.com>
2022-02-11 08:53:49 +0800

    1234

--- src/pages/redirect/index.tsx
+++ src/pages/redirect/index.tsx
@@ -2,10 +2,10 @@
  * @Author: 焦质晔
  * @Date: 2021-07-06 12:54:20
  * @Last Modified by: 焦质晔
- * @Last Modified time: 2021-07-13 22:01:27
+ * @Last Modified time: 2022-02-11 08:44:03
  */
 import React, { Component } from 'react';
-import { withRouter, Redirect } from 'react-router-dom';
+import { withRouter } from 'react-router-dom';
 
 @withRouter
 class Redirect extends Component<any> {
