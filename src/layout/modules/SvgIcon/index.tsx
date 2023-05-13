60894ee78b158d4cc404235792590a5c8c110d7f
jzy <jiaozhiye@163.com>
2023-05-13 14:41:53 +0800

    1234

--- src/layout/modules/SvgIcon/index.tsx
+++ src/layout/modules/SvgIcon/index.tsx
@@ -2,7 +2,7 @@
  * @Author: 焦质晔
  * @Date: 2023-05-13 13:48:35
  * @Last Modified by: 焦质晔
- * @Last Modified time: 2023-05-13 13:51:21
+ * @Last Modified time: 2023-05-13 14:37:07
  */
 import React from 'react';
 import classNames from 'classnames';
@@ -10,6 +10,11 @@ import classNames from 'classnames';
 type IProps = {
   svgUrl: string;
   className?: string;
+};
+
+const isSVGFormat = (str: string) => {
+  const svgRegex = /^<svg[^>]*>[^]*<\/svg>$/i;
+  return svgRegex.test(str);
 };
 
 const SvgIcon: React.FC<IProps> = (props) => {
@@ -20,7 +25,9 @@ const SvgIcon: React.FC<IProps> = (props) => {
     try {
       const res = await fetch(svgUrl);
       const svgText = await res.text();
-      setSvgContent(svgText);
+      if (isSVGFormat(svgText.trim())) {
+        setSvgContent(svgText);
+      }
     } catch (err) {
       // ...
     }
